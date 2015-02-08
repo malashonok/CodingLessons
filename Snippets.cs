e2caaf5c427814e5fc530c2041c04492b12068df
David Malashonok <david.malashonok@gmail.com>
2015-02-08 19:56:33 +0100

    Updated project file

--- Snippets.cs
+++ Snippets.cs
@@ -11,8 +11,8 @@ namespace CodingLessons
 {
     public static class Snippets
     {
-        //public static string stringOfConnection = Pwds.webConString;
-        public static string stringOfConnection = Pwds.localConString;
+        public static string stringOfConnection = Pwds.webConString;
+        //public static string stringOfConnection = Pwds.localConString;
         public static void CheckAdmin(IPrincipal User, HttpServerUtility Server)
         {
             if (User.IsInRole("admin"))
